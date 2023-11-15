46557a665ed16988ebc940d2bf8c61a6eca0c17c
Damo <daiguimao@sightp.com>
2023-11-15 18:35:20 +0800

    complete

--- src/sse/pages/index/index.js
+++ src/sse/pages/index/index.js
@@ -15,9 +15,7 @@ Page({
       method: "GET",
       dataType: "arraybuffer",
       enableChunked: true,
-      header: {
-        "Content-Type": "text/event-stream"
-      },
+      header: {},
       success: function (res) {
         // 监听 onMessage 事件处理 SSE 数据
         console.log(`success`, res);
@@ -36,6 +34,56 @@ Page({
         let obj = JSON.parse(j);
         console.log(`json`, j);
         console.log(`obj`, obj);
+      }
+      if (~message.indexOf("[DONE]")) {
+        // 判断是否是最后一条消息
+        console.log("done");
+        // 关闭 SSE 连接
+        RequestTask.offChunkReceived();
+      }
+    });
+  },
+  tapGpt() {
+    // 发送 SSE 请求
+    let RequestTask = wx.request({
+      url: "http://localhost:9090/zy/chat/openId",
+      method: "POST",
+      dataType: "arraybuffer",
+      enableChunked: true,
+      data: {
+        text: "您好，蚕丛及⻥凫",
+        useMouthShape: false,
+        stream: true,
+        sessionId: null
+      },
+      header: {
+        Authorization:
+          "MjBlYjBkNzhjNjgyMTMwMWI5MWRlZGU3YzAxMWQ0NjhmM2FmZTE2OWM0MDEzYmRjNGNmMTU1NzY1MWIyODg3M3siYXBwS2V5IjoiMTlmOWVkMGE0YWViNGZiMmEyZjNmMmY0NzE3ZTM4OTkiLCJleHBpcmVzIjoxNzMwNTM0NjU5NDk0fQ=="
+      },
+      success: function (res) {
+        // 监听 onMessage 事件处理 SSE 数据
+        console.log(`success`, res);
+        RequestTask.offChunkReceived();
+        RequestTask = null;
+      }
+    });
+    RequestTask.onChunkReceived(function (event) {
+      // 处理接收到的消息
+      const uint8Array = new Uint8Array(event.data);
+      // let message = String.fromCharCode.apply(null, uint8Array);
+      let message = decodeURIComponent(escape(String.fromCharCode.apply(null, uint8Array)));
+      console.log(`message`, message);
+      let idx = message.indexOf("data:");
+      if (idx > -1) {
+        let j = message.substring(idx + 5);
+        try {
+          let obj = JSON.parse(j);
+          console.log(`json`, j);
+          console.log(`obj`, obj);
+        } catch (error) {
+          console.error(error);
+          console.error(j);
+        }
       }
       if (~message.indexOf("[DONE]")) {
         // 判断是否是最后一条消息
